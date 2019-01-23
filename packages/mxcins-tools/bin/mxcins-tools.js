3a19f1804dbff5e8f422db11712502a284ce7172
Xiaochuan <mxcins@gmail.com>
2019-01-23 22:24:03 +0800

    add lock

---
+++ packages/mxcins-tools/bin/mxcins-tools.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const signale = require('signale');
+const argv = require('yargs-parser')(process.argv.slice(2));
+
+if (argv.v || argv.version) {
+  signale.log(require('../package').version);
+  process.exit(0);
+}
+
+switch (args._[0]) {
+  case 'build':
+  case 'test':
+  case 'rollup':
+    // require(`./src/${args._}`);
+    break;
+  default:
+    signale.error(`Unknown command ${args._}`);
+    break;
+}
