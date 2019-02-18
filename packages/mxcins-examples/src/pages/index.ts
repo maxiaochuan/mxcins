6fd0b91a2d91c6cec63618795797c9a8efc21621
Xiaochuan <mxcins@gmail.com>
2019-02-19 01:08:58 +0800

    update

--- packages/mxcins-examples/src/pages/index.ts
+++ packages/mxcins-examples/src/pages/index.ts
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import { arr2obj } from '@mxcins/lodash';
 
-console.log(arr2obj);
-
 export default class Index extends React.Component {
+  componentDidMount() {
+    const arr = [
+      { id: 1, name: 'xiaochuan' },
+    ];
+    console.log(arr2obj(arr));
+  }
   render() {
     return '1';
   }
