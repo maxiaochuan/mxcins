04c760867806d3ac07ec1ed2c856670729b9dfcf
XiaochuanMa <mxcins@gmail.com>
2019-02-21 19:52:24 +0800

    update arr2obj

--- packages/mxcins-lodash/src/arr2obj.ts
+++ packages/mxcins-lodash/src/arr2obj.ts
@@ -7,9 +7,14 @@ export default function arr2obj<T extends { [x: string]: any }>(
   arr: T[],
   options: IArr2objOptions<T> = {},
 ) {
+  if (!Array.isArray(arr)) {
+    throw new Error('arr2obj first arg MUST be array');
+  }
   const { key = 'id', clone = false } = options;
   return arr.reduce<{ [x: string]: T }>((prev, item) => {
-    prev[item[key]] = clone ? { ...item } : item;
+    if (item && item[key]) {
+      prev[item[key]] = clone ? { ...item } : item;
+    }
     return prev;
   }, {});
 }
