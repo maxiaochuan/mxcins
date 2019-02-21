f807a7e959d512b83ab92b77cab65f8637510309
XiaochuanMa <mxcins@gmail.com>
2019-02-21 19:41:35 +0800

    update test

--- packages/mxcins-lodash/__tests__/isString.test.ts
+++ packages/mxcins-lodash/__tests__/isString.test.ts
@@ -4,19 +4,19 @@ describe('is string', () => {
   it('number', () => {
     expect(isString(100)).toBe(false);
   });
-  // it('string', () => {
-  //   expect(isString('1')).toBe(true);
-  //   expect(isString('')).toBe(true);
-  // });
-  // it('object', () => {
-  //   expect(isString({})).toBe(false);
-  // });
-  // it('function', () => {
-  //   expect(isString(() => 1)).toBe(false);
-  // });
-  // it('others', () => {
-  //   expect(isString(null)).toBe(false);
-  //   expect(isString(undefined)).toBe(false);
-  //   expect(isString(NaN)).toBe(false);
-  // });
+  it('string', () => {
+    expect(isString('1')).toBe(true);
+    expect(isString('')).toBe(true);
+  });
+  it('object', () => {
+    expect(isString({})).toBe(false);
+  });
+  it('function', () => {
+    expect(isString(() => 1)).toBe(false);
+  });
+  it('others', () => {
+    expect(isString(null)).toBe(false);
+    expect(isString(undefined)).toBe(false);
+    expect(isString(NaN)).toBe(false);
+  });
 });
