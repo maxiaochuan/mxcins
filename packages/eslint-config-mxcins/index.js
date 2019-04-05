a53af6c83c6a261f61ca87928494f919997821df
Xiaochuan Ma <mxcins@gmail.com>
2019-04-05 22:15:54 +0800

    lint config

---
+++ packages/eslint-config-mxcins/index.js
@@ -0,0 +1,34 @@
+module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      legacyDecorators: true,
+    },
+  },
+  extends: ['airbnb', 'prettier', 'plugin:compat/recommended'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    mocha: true,
+    jest: true,
+    jasmine: true,
+  },
+  rules: {
+    'quote-props': [2, 'consistent-as-needed'],
+    'no-param-reassign': [2, { props: false }],
+    'no-unused-expressions': [2, { allowShortCircuit: true, allowTernary: true }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
+    'react/jsx-wrap-multilines': 0,
+    'react/prop-types': 0,
+    'react/forbid-prop-types': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'import/no-unresolved': [2, { ignore: ['^@/', '^umi/', '^zeus', /^@/] }],
+    'import/no-extraneous-dependencies': [2, { optionalDependencies: true }],
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'linebreak-style': 0,
+  },
+};
