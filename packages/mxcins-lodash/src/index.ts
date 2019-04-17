32eb2bdd899cef03594baed7e4f9bf137f8a7bb3
Xiaochuan Ma <mxcins@gmail.com>
2019-04-17 15:42:59 +0800

    change arr2obj parseJson to utils

--- packages/mxcins-lodash/src/index.ts
+++ packages/mxcins-lodash/src/index.ts
@@ -1,9 +1,3 @@
 // base
 export { default as isString } from './isString';
 export { default as isObject } from './isObject';
-
-// arr
-export { default as arr2obj } from './arr2obj';
-
-// json
-export { default as parseJson } from './parseJson';
