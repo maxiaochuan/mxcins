66a3ebe40ef77ed47abc0fd4b5f8e416aee92ccb
Xiaochuan Ma <mxcins@gmail.com>
2020-04-22 15:21:28 +0800

    update

--- packages/mxcins-utils/__tests__/treehelper.test.ts
+++ packages/mxcins-utils/__tests__/treehelper.test.ts
@@ -23,48 +23,48 @@ describe('tree helper', () => {
 
   it('node parent', () => {
     const helper = new TreeHelper([...d1]);
-    expect(helper.nodes['1'].parent).toBe(helper.nodes['0']);
-    expect(helper.nodes['2'].parent).toBe(helper.nodes['1']);
+    expect(helper.nodes['1']?.parent).toBe(helper.nodes['0']);
+    expect(helper.nodes['2']?.parent).toBe(helper.nodes['1']);
   });
 
   it('node children', () => {
     const helper = new TreeHelper([...d1]);
-    expect(helper.nodes['0'].children?.length).toBe(1);
-    expect(helper.nodes['1'].children?.length).toBe(1);
-    expect(helper.nodes['2'].children).toBe(undefined);
+    expect(helper.nodes['0']?.children?.length).toBe(1);
+    expect(helper.nodes['1']?.children?.length).toBe(1);
+    expect(helper.nodes['2']?.children).toBe(undefined);
   });
 
   it('node ancestors', () => {
     const helper = new TreeHelper([...d1]);
-    expect(helper.nodes['0'].ancestors).toBe(undefined);
-    expect(helper.nodes['1'].ancestors).toBe(undefined);
-    expect(helper.nodes['2'].ancestors).toBe(undefined);
+    expect(helper.nodes['0']?.ancestors).toBe(undefined);
+    expect(helper.nodes['1']?.ancestors).toBe(undefined);
+    expect(helper.nodes['2']?.ancestors).toBe(undefined);
   });
 
   it('node proletariats', () => {
     const helper = new TreeHelper([...d1]);
-    expect(helper.nodes['0'].proletariats).toBe(undefined);
-    expect(helper.nodes['1'].proletariats).toBe(undefined);
-    expect(helper.nodes['2'].proletariats).toBe(undefined);
+    expect(helper.nodes['0']?.proletariats).toBe(undefined);
+    expect(helper.nodes['1']?.proletariats).toBe(undefined);
+    expect(helper.nodes['2']?.proletariats).toBe(undefined);
   });
 
   it('opts ancestors true', () => {
     const helper = new TreeHelper([...d1], { ancestors: true });
-    expect(helper.nodes['0'].ancestors).toBe(undefined);
-    expect(helper.nodes['1'].ancestors?.length).toBe(1);
-    expect(helper.nodes['1'].ancestors?.[0]).toBe(helper.nodes['0']);
-    expect(helper.nodes['2'].ancestors?.length).toBe(2);
-    expect(helper.nodes['2'].ancestors?.[0]).toBe(helper.nodes['0']);
-    expect(helper.nodes['2'].ancestors?.[1]).toBe(helper.nodes['1']);
+    expect(helper.nodes['0']?.ancestors).toBe(undefined);
+    expect(helper.nodes['1']?.ancestors?.length).toBe(1);
+    expect(helper.nodes['1']?.ancestors?.[0]).toBe(helper.nodes['0']);
+    expect(helper.nodes['2']?.ancestors?.length).toBe(2);
+    expect(helper.nodes['2']?.ancestors?.[0]).toBe(helper.nodes['0']);
+    expect(helper.nodes['2']?.ancestors?.[1]).toBe(helper.nodes['1']);
   });
 
   it('opts proletariats false', () => {
     const helper = new TreeHelper([...d1], { proletariats: true });
-    expect(helper.nodes['0'].proletariats?.length).toBe(1);
-    expect(helper.nodes['0'].proletariats?.[0]).toBe(helper.nodes['2']);
-    expect(helper.nodes['1'].proletariats?.length).toBe(1);
-    expect(helper.nodes['1'].proletariats?.[0]).toBe(helper.nodes['2']);
-    expect(helper.nodes['2'].proletariats).toBe(undefined);
+    expect(helper.nodes['0']?.proletariats?.length).toBe(1);
+    expect(helper.nodes['0']?.proletariats?.[0]).toBe(helper.nodes['2']);
+    expect(helper.nodes['1']?.proletariats?.length).toBe(1);
+    expect(helper.nodes['1']?.proletariats?.[0]).toBe(helper.nodes['2']);
+    expect(helper.nodes['2']?.proletariats).toBe(undefined);
   });
 
   it('opt uid, puid', () => {
@@ -76,23 +76,23 @@ describe('tree helper', () => {
 
     const h1 = new TreeHelper(dd1, { uid: 'key', puid: 'pid' });
 
-    expect(h1.nodes['1'].children?.length).toBe(1);
-    expect(h1.nodes['1'].children?.[0]).toBe(h1.nodes['2']);
+    expect(h1.nodes['1']?.children?.length).toBe(1);
+    expect(h1.nodes['1']?.children?.[0]).toBe(h1.nodes['2']);
 
-    const h2 = new TreeHelper(dd1, { uid: r => r.key, puid: r => r.pid });
+    const h2 = new TreeHelper(dd1, { uid: r => r.key, puid: r => r.pid as string });
 
-    expect(h2.nodes['1'].children?.length).toBe(1);
-    expect(h2.nodes['1'].children?.[0]).toBe(h2.nodes['2']);
+    expect(h2.nodes['1']?.children?.length).toBe(1);
+    expect(h2.nodes['1']?.children?.[0]).toBe(h2.nodes['2']);
 
     const h3 = new TreeHelper(dd2, { uid: 'key' });
 
-    expect(h3.nodes['1'].children?.length).toBe(1);
-    expect(h3.nodes['1'].children?.[0]).toBe(h3.nodes['2']);
+    expect(h3.nodes['1']?.children?.length).toBe(1);
+    expect(h3.nodes['1']?.children?.[0]).toBe(h3.nodes['2']);
 
     const h4 = new TreeHelper(dd2, { uid: r => r.key });
 
-    expect(h4.nodes['1'].children?.length).toBe(1);
-    expect(h4.nodes['1'].children?.[0]).toBe(h4.nodes['2']);
+    expect(h4.nodes['1']?.children?.length).toBe(1);
+    expect(h4.nodes['1']?.children?.[0]).toBe(h4.nodes['2']);
   });
 
   it('insert', () => {
@@ -121,9 +121,9 @@ describe('tree helper', () => {
   it('update', () => {
     const h1 = new TreeHelper(d1);
 
-    expect(h1.nodes['0'].name).toBe('n0');
+    expect(h1.nodes['0']?.name).toBe('n0');
 
     h1.update('0', { name: 'new' });
-    expect(h1.nodes['0'].name).toBe('new');
+    expect(h1.nodes['0']?.name).toBe('new');
   });
 });
