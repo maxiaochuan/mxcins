8194fadb0ce09bc5ad2a1765d40590212212b2cf
Xiaochuan Ma <mxcins@gmail.com>
2020-06-28 18:07:27 +0800

    update theme plugin

--- packages/mxcins-theme/src/plugin.ts
+++ packages/mxcins-theme/src/plugin.ts
@@ -2,55 +2,85 @@ import { IApi } from 'umi';
 import { existsSync, readFileSync, writeFileSync } from 'fs';
 import serveStatic from 'serve-static';
 import { join } from 'path';
+import { camelcaseKeys as camel } from '@mxcins/utils';
 import build from './build';
 
+export interface IDynamicTheme extends Partial<Record<string, Record<string, string>>> {
+  default?: Record<string, string>;
+}
+
+export interface IDynamicThemeConfig {
+  theme: IDynamicTheme;
+}
+
 export default (api: IApi) => {
-  const { paths } = api;
+  const { paths, utils } = api;
   if (!paths.cwd || !paths.absNodeModulesPath || !paths.absOutputPath) {
     return;
   }
-  const { winPath, rimraf, mkdirp } = api.utils;
-  const themeConfigPath = winPath(join(paths.cwd, 'config/theme.config.json'));
+  const { winPath, rimraf, mkdirp } = utils;
+
+  // theme.config.json
+  const themeConfigPath = winPath(join(paths.cwd, 'config', 'theme.config.json'));
   if (!existsSync(themeConfigPath)) {
     return;
   }
-  const parsed = JSON.parse(readFileSync(themeConfigPath, 'utf-8'));
-  const themes = parsed.theme;
-  // development
-  const themeTempPath = winPath(join(paths.absNodeModulesPath, '.plugin-theme'));
-  api.addMiddewares(() => {
-    return serveStatic(themeTempPath);
+
+  const conf: IDynamicThemeConfig = JSON.parse(readFileSync(themeConfigPath, 'utf-8'));
+  const { theme } = conf;
+
+  api.modifyDefaultConfig(memo => {
+    return {
+      ...memo,
+      theme: theme.default,
+    };
   });
+
+  // development
+  const devThemeTempPath = winPath(join(paths.absNodeModulesPath, '.plugin-theme'));
+  api.addMiddewares(() => {
+    return serveStatic(devThemeTempPath);
+  });
+
+  api.addHTMLHeadScripts(() => [
+    {
+      content: `window.dynamic_theme = ${JSON.stringify(camel(theme, { deep: true }))}`,
+    },
+  ]);
+
+  try {
+    if (existsSync(devThemeTempPath)) {
+      rimraf.sync(devThemeTempPath);
+    }
+    if (existsSync(winPath(join(devThemeTempPath, 'theme')))) {
+      rimraf.sync(winPath(join(devThemeTempPath, 'theme')));
+    }
+    mkdirp.sync(devThemeTempPath);
+    mkdirp.sync(winPath(join(devThemeTempPath, 'theme')));
+  } catch (error) {
+    // console.log(error);
+  }
+
   api.onDevCompileDone(() => {
-    api.logger.info(`cache in: ${themeTempPath}`);
     api.logger.info('build theme');
 
-    try {
-      if (existsSync(themeTempPath)) {
-        rimraf.sync(themeTempPath);
-      }
-      if (existsSync(winPath(join(themeTempPath, 'theme')))) {
-        rimraf.sync(winPath(join(themeTempPath, 'theme')));
-      }
-      mkdirp.sync(themeTempPath);
-      mkdirp.sync(winPath(join(themeTempPath, 'theme')));
-    } catch (error) {
-      // console.log(error);
-    }
-
-    const cachePath = winPath(join(themeTempPath, 'cache.json'));
+    const cachePath = winPath(join(devThemeTempPath, 'cache.json'));
     if (!existsSync(cachePath)) {
       writeFileSync(cachePath, '');
     }
-    const current = readFileSync(themeConfigPath, 'utf-8');
+
+    const current = JSON.stringify(theme);
     const cache = readFileSync(cachePath, 'utf-8');
     if (cache !== current) {
+      api.logger.info(`themes changed`);
       build(
-        { outputPath: winPath(join(themeTempPath, 'theme')) },
-        Object.keys(themes).map(k => ({
-          fileName: `${k}.css`,
-          modifyVars: themes[k],
-        })),
+        { outputPath: winPath(join(devThemeTempPath, 'theme')) },
+        Object.keys(theme)
+          .filter(k => k !== 'default')
+          .map(k => ({
+            fileName: `${k}.css`,
+            modifyVars: theme[k] || {},
+          })),
       ).then(() => {
         api.logger.info('build theme success');
         writeFileSync(cachePath, current);
@@ -84,10 +114,12 @@ export default (api: IApi) => {
 
     build(
       { outputPath },
-      Object.keys(themes).map(k => ({
-        fileName: `${k}.css`,
-        modifyVars: themes[k],
-      })),
+      Object.keys(theme)
+        .filter(k => k !== 'default')
+        .map(k => ({
+          fileName: `${k}.css`,
+          modifyVars: theme[k] || {},
+        })),
     ).then(() => {
       api.logger.info('build theme success');
     });
