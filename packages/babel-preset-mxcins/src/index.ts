6ec1837472c6002e49b6a7cffa9055c0c5f60714
Xiaochuan Ma <mxcins@gmail.com>
2020-12-17 17:48:56 +0800

    feat(babel-preset-mxcins): upgrade babel-preset-mxcins
    affects: babel-preset-mxcins

--- packages/babel-preset-mxcins/src/index.ts
+++ packages/babel-preset-mxcins/src/index.ts
@@ -1,17 +1,37 @@
 /* eslint-disable global-require, @typescript-eslint/no-var-requires, @typescript-eslint/explicit-module-boundary-types */
 import type { Options as ENV } from '@babel/preset-env';
 
-export interface IOpts {
+interface ImportPluginOpts {
+  libraryName: string;
+  libraryDirectory?: string;
+  style?: boolean;
+  camel2DashComponentName?: boolean;
+}
+
+export interface PresetOpts {
   env?: ENV;
   debug?: boolean;
   react?: boolean | Record<string, unknown>;
   typescript?: boolean | Record<string, unknown>;
-  transformRuntime?: boolean;
+  transformRuntime?: boolean | Record<string, unknown>;
+  styledComponents?: boolean | Record<string, unknown>;
+  import?: ImportPluginOpts[];
 }
 
 const toObject = (input: unknown) => (typeof input === 'object' ? input : {});
 
-export default (_: unknown, { debug, env, react, typescript, transformRuntime }: IOpts = {}) => ({
+export default (
+  _: unknown,
+  {
+    debug,
+    env,
+    react,
+    typescript,
+    transformRuntime,
+    styledComponents,
+    import: imports,
+  }: PresetOpts = {},
+) => ({
   presets: [
     [
       require('@babel/preset-env').default,
@@ -47,14 +67,23 @@ export default (_: unknown, { debug, env, react, typescript, transformRuntime }:
     transformRuntime && [
       require('@babel/plugin-transform-runtime').default,
       {
-        corejs: 3,
         version: require('@babel/runtime/package.json').version,
         // https://github.com/umijs/umi/blob/master/packages/babel-preset-umi/src/index.ts
         // 2020-03-30 01:07:20 在 yarn link 之后路径会有问题 打包dependency 的 import 很奇怪 暂时去掉
         // absoluteRuntime: dirname(require.resolve('@babel/runtime/package.json')),
         // https://babeljs.io/docs/en/babel-plugin-transform-runtime#useesmodules
         useESModules: true,
+        ...toObject(transformRuntime),
       },
+      styledComponents && [
+        require('babel-plugin-styled-components').default,
+        { ssr: false, ...toObject(styledComponents) },
+      ],
+      ...(imports
+        ? imports.map(o => {
+            return [require('babel-plugin-import').default, o, o.libraryName];
+          })
+        : []),
     ],
   ].filter(Boolean),
 });
