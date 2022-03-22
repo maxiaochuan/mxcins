8d57ed868aef570344a0b2be9786997096877322
maxiaochuan <mxcins@gmail.com>
2022-03-22 14:35:07 +0800

    feat(webapi): export breakpoint type

--- packages/webapi/src/index.ts
+++ packages/webapi/src/index.ts
@@ -1,5 +1,7 @@
-import * as SingleResizeObserver from './single-resize-observer/MxLibs__SingleResizeObserver.gen'
+import * as SingleResizeObserver from './single-resize-observer/MxLibs__SingleResizeObserver.gen';
 
-import * as BreakpointSub from './breakpoint-sub/MxLibs__BreakpointSub.gen'
+import * as BreakpointSub from './breakpoint-sub/MxLibs__BreakpointSub.gen';
 
-export { SingleResizeObserver, BreakpointSub }
+export type { BreakpointType } from './breakpoint-sub/MxLibs__BreakpointSub.gen';
+
+export { SingleResizeObserver, BreakpointSub };
