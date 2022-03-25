833ffc4a6fa86fdc3ab07b09110e7ee6129214e2
maxiaochuan <mxcins@gmail.com>
2022-03-25 17:52:21 +0800

    feat(webapi): resize observer polifill

--- packages/webapi/src/index.ts
+++ packages/webapi/src/index.ts
@@ -1,3 +1,5 @@
+import './polyfill'
+
 import * as SingleResizeObserver from './single-resize-observer/MxWebapi__SingleResizeObserver.gen';
 
 import * as BreakpointSub from './breakpoint-sub/MxWebapi__BreakpointSub.gen';
