83f704e2fd5b09c398aaa282033fd2e619500749
Xiaochuan Ma <xiaochuanma@Xiaochuans-MacBook-Pro.local>
2022-03-26 04:43:54 +0800

    feat(components): input password

---
+++ packages/components/src/input/MxRC__Input__Password.gen.tsx
@@ -0,0 +1,14 @@
+/* TypeScript file generated from MxRC__Input__Password.res by genType. */
+/* eslint-disable import/first */
+
+
+import * as React from 'react';
+
+// @ts-ignore: Implicit any on import
+import * as MxRC__Input__PasswordBS__Es6Import from './MxRC__Input__Password.bs';
+const MxRC__Input__PasswordBS: any = MxRC__Input__PasswordBS__Es6Import;
+
+// tslint:disable-next-line:interface-over-type-literal
+export type Props = {};
+
+export const make: React.ComponentType<{}> = MxRC__Input__PasswordBS.make;
