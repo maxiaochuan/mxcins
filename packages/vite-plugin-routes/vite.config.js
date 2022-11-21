c2ba45d73a45899d49fb498f6a2b20c6b41776d1
maxiaochuan <mxcins@gmail.com>
2022-11-21 21:45:02 +0800

    chore: storybook

---
+++ packages/vite-plugin-routes/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import packageJSON from './package.json';
+
+export default defineConfig({
+  build: {
+    minify: false,
+    lib: {
+      entry: 'src/index.ts',
+      fileName: 'index',
+      formats: ['cjs'],
+    },
+    rollupOptions: {
+      external: [
+        'fs',
+        'path',
+        ...Object.keys(packageJSON.dependencies),
+      ],
+    },
+  },
+});
